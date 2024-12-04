4dccc8c63be7aac167a4a7422e4a34c4eff86bbc
Marwa Hemdan <marwahemdan4410@yahoo.com>
2024-12-04 10:46:38 -0500

    change form attribute

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -25,7 +25,12 @@
           :counter="100"
           label="Your Message"
         ></v-textarea>
-        <v-btn :disabled="!invalid" color="success" type="submit" @click="addMessage" block
+        <v-btn
+          :disabled="!invalid"
+          color="success"
+          type="submit"
+          @click="addMessage"
+          block
           >Submit</v-btn
         >
         <v-btn
@@ -43,23 +48,44 @@
           Sorry, there is something wrong
         </h5>
         <h5 class="warningClass text-center mt-5" v-if="repeatMessage">
-          You have already sent a message, please wait 
+          You have already sent a message, please wait
         </h5>
       </v-form>
     </v-sheet>
+    <div class="pa-4 text-center">
+      <v-dialog max-width="500" v-model="dialogSuccess" persistent>
+        <v-card>
+          <v-card-text>
+            <h3 class="addClass">You sent your message successfully</h3>
+          </v-card-text>
+          <div class="text-center">
+            <img
+              src="https://static.vecteezy.com/system/resources/previews/008/506/390/original/bright-green-tick-checkmark-icon-free-png.png"
+              class="images"
+            />
+          </div>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn text="Close Dialog" @click="reset"></v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </div>
   </div>
 </template>
 <script>
 import { collection, addDoc, getDocs } from "firebase/firestore";
-import {db} from '../firebase.js'
+import { db } from "../firebase.js";
 export default {
   data: () => ({
+    dialogSuccess: true,
+    loading: false,
     firstName: "",
-  
+
     firstNameRules: [
       (value) => {
         if (value?.length >= 10) return true;
-        return "First name must be at least 3 characters.";
+        return "First name must be at least 10 characters.";
       },
     ],
 
@@ -71,42 +97,50 @@ export default {
     subText: "",
     loading: false,
     failedAdd: false,
-    description:''
-    ,repeatMessage:false
+    description: "",
+    repeatMessage: false,
   }),
-  methods:{
-   async  addMessage(){
-     const querySnapshot = await getDocs(collection(db, "users"));
+  methods: {
+    async addMessage() {
+      const querySnapshot = await getDocs(collection(db, "users"));
+      this.loading = true;
       querySnapshot.forEach((doc) => {
-        if(doc.data().email == this.email)
-        { this.repeatMessage=true
-        }
-        else
-        {
-          this.addNewMessage()
-          
+        if (doc.data().email == this.email) {
+          this.repeatMessage = true;
+          this.loading = false;
+        } else {
+          this.addNewMessage();
         }
       });
-
-    
-
     },
- async   addNewMessage(){
-        const colRef = collection(db, 'users')
+    async addNewMessage() {
+      const colRef = collection(db, "users");
       // data to send
       const dataObj = {
         firstName: this.firstName,
         email: this.email,
         message: this.description,
-       createdAt: new Date().getTime().toString(),
-      }
+        createdAt: new Date().getTime().toString(),
+      };
 
       // create document and return reference to it
-      const docRef = await addDoc(colRef, dataObj)
-
+      const docRef = await addDoc(colRef, dataObj);
+      this.dialogSuccess = true;
+      this.loading = false;
       // access auto-generated ID with '.id'
-      console.log('Document was created with ID:', docRef.id)
-    }
-  }
+      console.log("Document was created with ID:", docRef.id);
+    },
+    reset() {
+      this.dialogSuccess = false;
+      this.$refs.form.reset();
+      this.loading = false;
+    },
+  },
 };
 </script>
+<style>
+.images {
+  width: 300px;
+  height: 300px;
+}
+</style>
